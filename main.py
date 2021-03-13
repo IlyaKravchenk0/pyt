acc3dc2ef0a5ab74fa8f19593dde56bc95b4b2cc
IlyaKravchenk0 <80105348+IlyaKravchenk0@users.noreply.github.com>
2021-03-13 09:17:46 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+multiplication = 15 * 3
+division  = 15 / 3
+division_without_remainder = 15 // 2
+degree = 15 ** 2
+
+print(f'multiplication = {type(multiplication)}\n'
+      f'division = {type(division)}\n'
+      f'division_without_remainder = {type(division_without_remainder)}\n'
+      f'degree = {type(degree)}\n'
+      )
